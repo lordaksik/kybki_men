f4e66e8b67ab3ec0c3c9562d3c598d67a68163d4
lordaksik <48729120+lordaksik@users.noreply.github.com>
2022-04-25 13:09:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,82 +9,67 @@ bot.start((ctx) => {
 })
 bot.help((ctx) => ctx.reply('Напиши /bot '))
 bot.hears('/bot', async (ctx) => {
-   async function request2() {
+       async function request2() {
 
-      const response = await fetch("https://betgames9.betgames.tv/web/v2/games/results/testpartner/en/0/2020-27-07/7/1/")
-      const data = await response.json()
-      let arr = [];
-      let num=0;
-      let num2=0;
-      let arrs = [];
-      let arrs2 = [];
-      let kol=9;
-      for (let i = 0; i <= kol; i++) {
-         let number = data.items.results[i].results[0].number
-         if(number<10||number==10)
-         {
-            num=num+1;
-         }
-      }
-      if(num===10){
-         console.log(kol+" "+num)
-         ctx.reply("Сектор с числом БОЛЬШЕ 9.5 не падал 10 раз");
-      }
-      for (let i = 0; i <= kol; i++) {
-         let number2 = data.items.results[i].results[0].number
-         
-         if(number2>10)
-         {
-            num2=num2+1;
-         }
-      }
-      if(num2==10){
-         ctx.reply("Сектор с числом МЕНЬШЕ 9.5 не падал 10 раз");
-      }
-
-      for (let i = 0; i <= 14; i++) {
-
-         colors = data.items.results[i].results[0].color
-
-         arr.push(colors);
-      }
-      if (arr.includes('grey') === false) {
-         ctx.reply("Серого цвета не было 14 раз");
-      }
-      if (arr.includes('black') === false) {
-         ctx.reply("Чёрного цвета не было 14 раз");
-      }
-
-      if (arr.includes('red') === false) {
-         ctx.reply("Красного цвета не было 14 раз");
-      }
-
-
-      for (let i = 0; i <= 29; i++) {
-         colorss = data.items.results[i].results[0].color
-         arrs.push(colorss);
-      }
-      if (arrs.includes('white') === true) { global.firstch = 2; }
-      else {
-         global.firstch = 0;
-      }
-
-      const response2 = await fetch("https://betgames9.betgames.tv/web/v2/games/results/testpartner/en/0/2020-27-07/7/2/")
-      const data2 = await response2.json()
-       for (let i = 0; i <= 29; i++) {
-         colors_cyb = data2.items.results[i].results[0].color
-         arrs2.push(colors_cyb);
-      }
-         if (arrs2.includes('white') === true) { global.firstch_two = 2; }
-      else {
-         global.firstch_two = 0;
-      }
-      if (global.firstch === 0 && global.firstch_two === 0) {
-         ctx.reply("Кубка не было 60 раз");
-      }
-
-
-   }
+          let data = [];
+          let response = [];
+          let sum = [];
+          let sum2 = [];
+          let res = 0;
+          let result = 0;
+          let colors_cyb, colors_cyb2;
+          for (j = 1; j < 31; j++) {
+             response[j] = await fetch("https://betgames9.betgames.tv/web/v2/games/results/testpartner/en/0/2020-27-07/7/" + j + '/')
+             data[j] = await response[j].json()
+             if (data[j].items.page !== j) {
+                break;
+             }
+             for (i = 0; i < data[j].items.results.length; i++) {
+                colors_cyb = data[j].items.results[i].results[0].color
+                sum.push(colors_cyb);
+             }
+          }
+          for (i = 0; i < sum.length; i++) {
+             if (sum[i] === 'white' && sum[i + 1] === 'white') {
+                result = 1;
+                break;
+             }
+             if (sum[i] === 'white') {
+                res++;
+             }
+          }
+          if (result === 0) {
+             res = 0;
+             var now = new Date();
+             now.setDate(now.getDate() - 1)
+             console.log(now.getFullYear() + '-0' + (now.getMonth() + 1) + '-' + now.getDate())
+             let dat = now.getFullYear() + '-0' + (now.getMonth() + 1) + '-' + now.getDate();
+             for (k = 1; k < 31; k++) {
+                response[k] = await fetch("https://betgames9.betgames.tv/web/v2/games/results/testpartner/en/0/" + dat + "/7/" + k + '/')
+                data[k] = await response[k].json()
+                if (data[k].items.page !== k) {
+                   break;
+                }
+                for (i = 0; i < data[k].items.results.length; i++) {
+                   colors_cyb2 = data[k].items.results[i].results[0].color
+                   sum.push(colors_cyb2);
+                }
+             }
+             //console.log(sum)
+             for (i = 0; i < sum.length; i++) {
+                if (sum[i] === 'white' && sum[i + 1] === 'white') {
+                   break;
+                }
+                if (sum[i] === 'white') {
+                   res++;
+                }
+             }
+          }
+          console.log('Было ' + res + " кубков");
+          if (sum[0] === 'white' && res > 10) {
+             ctx.reply('Было ' + res + " кубков");
+          }
+       }
    function good() {
       ctx.reply( "Вы запустили Бота на стратегию «Колесо» ⚠ Не забудьте поставить особые уведомления на Бота, и ждите сигнала на валуйные ситуации!");
        ctx.reply( "Удачи! По всем вопросам пишите @BetgamesTV_Admin"); 
@@ -108,6 +93,67 @@ bot.hears('/end', async (ctx) => {
       ctx.reply("Этот бот и так выключен");
    }
 })
+bot.hears('/kol', async (ctx) => {
+   async function inform() {
 
+      let data = [];
+      let response = [];
+      let sum = [];
+      let res = 0;
+      let result = 0;
+      let colors_cyb, colors_cyb2;
+      for (j = 1; j < 31; j++) {
+         response[j] = await fetch("https://betgames9.betgames.tv/web/v2/games/results/testpartner/en/0/2020-27-07/7/" + j + '/')
+         data[j] = await response[j].json()
+         if (data[j].items.page !== j) {
+            break;
+         }
+         for (i = 0; i < data[j].items.results.length; i++) {
+            colors_cyb = data[j].items.results[i].results[0].color
+            sum.push(colors_cyb);
+         }
+      }
+      for (i = 0; i < sum.length; i++) {
+         if (sum[i] === 'white' && sum[i + 1] === 'white') {
+            result = 1;
+            break;
+         }
+         if (sum[i] === 'white') {
+            res++;
+         }
+      }
+      //console.log(sum)
+      if (result === 0) {
+         res = 0;
+         var now = new Date();
+         now.setDate(now.getDate() - 1)
+         console.log(now.getFullYear() + '-0' + (now.getMonth() + 1) + '-' + now.getDate())
+         let dat = now.getFullYear() + '-0' + (now.getMonth() + 1) + '-' + now.getDate();
+         for (k = 1; k < 31; k++) {
+            response[k] = await fetch("https://betgames9.betgames.tv/web/v2/games/results/testpartner/en/0/" + dat + "/7/" + k + '/')
+            data[k] = await response[k].json()
+            if (data[k].items.page !== k) {
+               break;
+            }
+            for (i = 0; i < data[k].items.results.length; i++) {
+               colors_cyb2 = data[k].items.results[i].results[0].color
+               sum.push(colors_cyb2);
+            }
+         }
+         //console.log(sum)
+         for (i = 0; i < sum.length; i++) {
+            if (sum[i] === 'white' && sum[i + 1] === 'white') {
+               break;
+            }
+            if (sum[i] === 'white') {
+               res++;
+            }
+         }
+      }
+      ctx.reply('Было ' + res + " кубков");
+
+   }
+inform()
+})
 bot.launch()
 
